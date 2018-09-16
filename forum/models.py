cf023fd27f5f2d8c0d85ba207d86e3cd37b33f59
Simen Holmestad <simen@holmestad.com>
2018-09-16 17:00:04 +0200

    Added model "ForumPost"

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class ForumPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    headline = models.CharField("Overskrift", max_length=40)
+    content = models.TextField("innhold", default="")
+    time_made = models.DateTimeField(default=timezone.now)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.headline + " av " + self.author.first_name + " " + self.author.last_name
--- forum/tests.py
+++ forum/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import ForumPost
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ForumPostModelTestCase(TestCase):
+    def test_str(self):
+        test_user = User(first_name="Test", last_name="Testesen")
+        forum_post = ForumPost(author=test_user, headline="Halla balla", content="")
+        self.assertEqual(str(forum_post), "Halla balla av Test Testesen")
